aa981b4322b78cc1636fd1c07f028341ff9505d5
mattSang <13121864164@163.com>
2019-09-25 08:16:44 +0800

    Ribbon负载均衡

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -5,13 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RetryRule;
-
 @Configuration
 public class ConfigBean //boot -->spring   applicationContext.xml --- @Configuration配置   ConfigBean = applicationContext.xml
 { 
 	@Bean
+	@LoadBalanced//Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端 负载均衡的工具
 	public RestTemplate getRestTemplate()
 	{
 		return new RestTemplate();
